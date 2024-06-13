c71eedfffb1cc9665c3e872c388c0f1234a20324
nikhilnishadoo7 <rrnrrn007@gmail.com>
2024-06-14 02:20:31 +0530

    adding the link tag in bannar

--- src/compontents/BannerHome.js
+++ src/compontents/BannerHome.js
@@ -69,9 +69,11 @@ const BannerHome = () => {
                   View:{(data.popularity).toFixed(0)}
                 </p>
               </div>
-              <Link to={"/"+data?.media_type+"/"+data?.id} className='bg-white px-4 py-2 text-black font-bold rounded mt-4 hover:bg-gradient-to-l from-red-700 to-orange-500 shadow-md transition-all hover:scale-100'>
+                  <Link  to={"/"+data?.media_type+"/"+data?.id}>
+                  <button className='bg-white px-4 py-2 text-black font-bold rounded mt-4 hover:bg-gradient-to-l from-red-700 to-orange-500 shadow-md transition-all hover:scale-100'>
                   PLAY NOW
-                </Link>
+                </button>
+                  </Link>
             </div>
             </div>
            
